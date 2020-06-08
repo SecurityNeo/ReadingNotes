5cdb3186aa5a077d6acee9771deacb788ef61978
SecurityNeo <lihu_security@163.com>
2020-06-08 22:37:09 +0800

    Coding/Go/数据结构和算法/单链表

--- Coding/Go/数据结构和算法/单链表.go
+++ Coding/Go/数据结构和算法/单链表.go
@@ -11,17 +11,19 @@ import "fmt"
 	1）新数据总是在链表尾部插入（insertHero）
 	2）新数据按照ID从小到大插入（insertHeroByID）
 		注意：一定要先让新加入节点先指向下一个节点，然后再让其前面一个节点指向新加入节点，顺序颠倒的话链表就断了。
+5、删除节点：先找到待删除节点，将其前一个节点的next指向后一个节点即可
 */
 
 // 示例： 使用单链表实现水浒英雄管理
 
 type HeroNode struct {
-	ID   int
+	ID int
 	name string
 	next *HeroNode
 }
 
-func insertHero(head *HeroNode, newHeroNode *HeroNode) {
+
+func insertHero(head *HeroNode, newHeroNode *HeroNode){
 	// 创建辅助节点，类似于辅助指针，用于链表节点的定位，辅助节点先指向头节点
 	tempNode := head
 	for {
@@ -29,7 +31,7 @@ func insertHero(head *HeroNode, newHeroNode *HeroNode) {
 		if tempNode.next == nil {
 			tempNode.next = newHeroNode
 			break
-		} else {
+		}else {
 			// 辅助节点不断往后移
 			tempNode = tempNode.next
 		}
@@ -44,19 +46,34 @@ func insertHeroByID(head *HeroNode, newHeroNode *HeroNode) {
 			tempNode.next = newHeroNode
 			break
 			// 如果顺序为从大到小，修改">"为"<"即可
-		} else if tempNode.next.ID > newHeroNode.ID {
+		}else if tempNode.next.ID > newHeroNode.ID {
 			newHeroNode.next = tempNode.next
 			tempNode.next = newHeroNode
 			break
-		} else if tempNode.next.ID == newHeroNode.ID {
-			fmt.Printf("ID %d conflict!\n", newHeroNode.ID)
+		}else if tempNode.next.ID == newHeroNode.ID {
+			fmt.Printf("ID %d conflict!\n",newHeroNode.ID)
 			break
 		}
 		tempNode = tempNode.next
 	}
 }
 
-func showHero(head *HeroNode) {
+// 根据ID删除链表中的数据
+func deleteHeroNodeByID(head *HeroNode, id int){
+	tempNode := head
+	for {
+		if tempNode.next == nil {
+			fmt.Printf("ID %d does not exist! \n", id)
+			break
+		}else if tempNode.next.ID == id {
+			tempNode.next = tempNode.next.next
+			return
+		}
+		tempNode = tempNode.next
+	}
+}
+
+func showHero(head *HeroNode){
 	tempNode := head
 	if tempNode.next == nil {
 		fmt.Println("There is no hero!")
@@ -64,36 +81,39 @@ func showHero(head *HeroNode) {
 	}
 	for {
 		// 先打印下一个节点数据，然后辅助节点往后移，当后移之后辅助节点指向为空（tempNode.next），说明已完成整个链表的遍历
-		fmt.Printf("[%d %s] --> ", tempNode.next.ID, tempNode.next.name)
+		fmt.Printf("[%d %s] --> ",tempNode.next.ID,tempNode.next.name)
 		tempNode = tempNode.next
 		if tempNode.next == nil {
 			break
 		}
 	}
+	fmt.Println()
 }
 
 func main() {
 	head := &HeroNode{}
 	// 定义测试数据
 	hero1 := &HeroNode{
-		ID:   1,
+		ID: 1,
 		name: "宋江",
 	}
 	hero2 := &HeroNode{
-		ID:   2,
+		ID: 2,
 		name: "吴用",
 	}
 	hero3 := &HeroNode{
-		ID:   3,
+		ID: 3,
 		name: "卢俊义",
 	}
 	hero4 := &HeroNode{
-		ID:   3,
+		ID: 4,
 		name: "林冲",
 	}
 	insertHeroByID(head, hero1)
 	insertHeroByID(head, hero3)
 	insertHeroByID(head, hero2)
 	insertHeroByID(head, hero4)
+	showHero(head)
+	deleteHeroNodeByID(head, 4)
 	showHero(head)
 }
