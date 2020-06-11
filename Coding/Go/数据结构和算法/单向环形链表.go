7856fb0143979e4015f5650c60c75faff7ab605c
SecurityNeo <lihu_security@163.com>
2020-06-11 11:44:44 +0800

    Coding/Go/数据结构和算法/单向环形链表

--- Coding/Go/数据结构和算法/单向环形链表.go
+++ Coding/Go/数据结构和算法/单向环形链表.go
@@ -9,6 +9,11 @@ import (
 	1、与单链表非常相似，只是单向环形链表的最后一个节点再指向头节点（X.next = head）；
 	2、注意，单向环形链表的头节点是要存储数据的；
 	3、当链表中只有一个节点时，它需要指向自己(head.next = head)
+	4、需要定义两个辅助节点，与单项链表不一样的是，这里的头节点是要存储数据的。先将helper节点定位
+       到头节点的上一个节点，后边helper节点会跟随tempNode一起移动。
+	5、当需要删除的节点就是头节点时，需要将头节点的下一个节点设置为新的头节点。我们这里所有的逻辑
+       都基于有一个头节点，所以头节点不能为空。我们在函数内修改了头节点，就需要将新的头节点返回去，
+       后边的操作就必须用这里返回去的新头节点。
 */
 
 type HeroNode struct {
@@ -19,12 +24,14 @@ type HeroNode struct {
 
 
 func InsertHeroNode(head *HeroNode, newHeroNode *HeroNode) {
+	// 链表为空时，头节点的下一个节点指向自己即可
 	if head.next == nil {
 		head.ID = newHeroNode.ID
 		head.name = newHeroNode.name
 		head.next = head
 		return
 	}
+	// 同样定义一个辅助节点，用来查找谁指向了头节点
 	tempNode := head
 	for {
 		if tempNode.next == head {
@@ -46,8 +53,10 @@ func deleteHeroNode(head *HeroNode, ID int) *HeroNode{
 		head.next = nil
 		return head
 	}
+	// 这里需要定义两个辅助节点，与单项链表不一样的是，这里的头节点是要存储数据的。
 	tempNode := head
 	helperNode := head
+	// 先将helper节点定位到头节点的上一个节点，后边helper节点会跟随tempNode一起移动。
 	for {
 		if helperNode.next == head {
 			break
@@ -61,6 +70,9 @@ func deleteHeroNode(head *HeroNode, ID int) *HeroNode{
 			break
 		}
 		if tempNode.ID == ID {
+			// 这里需要特别注意，当需要删除的节点就是头节点时，需要将头节点的下一个节点设置为新的头节点。
+			// 我们这里所有的逻辑都基于有一个头节点，所以头节点不能为空。我们在函数内修改了头节点，就需要
+			// 将新的头节点返回去，后边的操作就必须用这里返回去的新头节点。
 			if tempNode == head {
 				head = head.next
 			}
